61f6739010603eb273d9b06645a1b2f34a291577
Muhamad Daffa Azfa Rabbani <muhamaddaffaazfarabbani@365.ugm.ac.id>
2023-11-17 15:36:22 +0700

    Update login ke azure

---
+++ Koneksi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace bebas
+{
+    class Koneksi
+    {
+        public SqlConnection GetConn()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString ="Server = tcp:databasesqlfitnesstrackers.database.windows.net, 1433; Database = database_fitnesstracker; Persist Security Info=False;User ID = Asoatram; Password=daffa2!34;" +
+         "MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;";
+            return conn;
+        }
+    }
+}

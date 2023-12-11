d5addf042bad2035344557abf5f5c6010b2fa7a1
Muhamad Daffa Azfa Rabbani <muhamaddaffaazfarabbani@365.ugm.ac.id>
2023-12-11 17:22:15 +0700

    Test

---
+++ Form6.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FitnessTrackers
+{
+    class UserId
+    {
+        public string Id { get; set; }
+        public string Pass { get; set; }
+        public UserId(string Id, string Pass) { 
+            this.Id = Id;
+            this.Pass = Pass;  
+        }
+    }
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UserId user = new UserId(textBox1.Text, textBox2.Text);
+        }
+    }
+}
